f5ce7ff42336b700ce565c2e575721fef67c7830
Justin De Ocampo <justindeocampo99@gmail.com>
2020-07-16 16:12:09 -0700

    Added text input, wani-kani auth working

--- popup.js
+++ popup.js
@@ -1,17 +1,49 @@
-document.addEventListener('DOMContentLoaded', function (){
-    document.querySelector('button').addEventListener('click',
-    onclick, false)
+/*Script which renders functions in the chrome extension "popup" */
 
-    function onclick (){
-        chrome.tabs.query({currentWindow: true, active: true},
-            function(tabs){
-                chrome.tabs.sendMessage(tabs[0].id, 'heyo', setCount)// does setCount function below
-            })
+/*Event listener which attaches this even handler once the DOM has loaded the content of a page */
+document.addEventListener(
+  "DOMContentLoaded",
+  function () {
+    ////////////////////////////////////////////////////////////////////////////
+    document.querySelector("button").addEventListener("click", onclick, false);
+
+    function onclick() {
+      chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, "heyo", setCount); // does setCount function below
+      });
     }
 
-    function setCount(res){
-        const div = document.createElement('div')
-        div.textContent = `${res.count} kanji, and ${res.check}` // adds a div with this on it
-        document.body.appendChild(div) // appends the new div above
+    function setCount(res) {
+      const div = document.createElement("div");
+      div.textContent = `${res.count} kanji, and ${res.check}`; // adds a div with this on it
+      document.body.appendChild(div); // appends the new div above
     }
-}, false)
+
+    //////////////////////////////////////////////////////////////////////////
+    /*Handles submission of API-Key by user */
+    document.getElementById("API-Submit").addEventListener("click", onSubmit);
+
+    /*On submit button, authorize users API key */
+    function onSubmit() {
+      var apiToken = document.getElementById("API-Input").value;
+
+      var apiEndpointPath = "subjects";
+
+      var requestHeaders = new Headers({
+        Authorization: "Bearer " + apiToken,
+      });
+      var apiEndpoint = new Request(
+        "https://api.wanikani.com/v2/" + apiEndpointPath,
+        {
+          method: "GET",
+          headers: requestHeaders,
+        }
+      );
+
+      fetch(apiEndpoint)
+        .then((response) => response.json())
+        .then((responseBody) => console.log(responseBody));
+    }
+  },
+  false
+);
