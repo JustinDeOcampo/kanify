9a859b3d24140f14a1a87675f2906ccde67bce1e
Kevin Weinhold <kweinhold22@gmail.com>
2020-07-16 11:39:53 -0700

    Kanji Count
    Is able to count # of Kanji on page at this point

--- contents.js
+++ contents.js
@@ -1,1 +1,10 @@
-alert('Kevin Step 1.')
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    const re = /[\u4e00-\u9faf]+/igm
+    const matches = document.documentElement.innerHTML.match(re)
+  
+    sendResponse({count: matches.length})
+
+
+
+
+})
---
+++ popup.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function (){
+    document.querySelector('button').addEventListener('click',
+    onclick, false)
+
+    function onclick (){
+        chrome.tabs.query({currentWindow: true, active: true},
+            function(tabs){
+                chrome.tabs.sendMessage(tabs[0].id, 'heyo', setCount)
+            })
+    }
+
+    function setCount(res){
+        const div = document.createElement('div')
+        div.textContent = `${res.count} kanji`
+        document.body.appendChild(div)
+    }
+}, false)
