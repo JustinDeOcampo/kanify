7c7d009bbe321317b191d31deb785b76308bfc47
Justin De Ocampo <justindeocampo99@gmail.com>
2020-07-31 14:08:34 -0700

    Made pop up close after confirmation

--- background.js
+++ background.js
@@ -1,4 +1,5 @@
 chrome.storage.sync.get("user_token", function (storage_data) {
+  //BUG: user has to double click
   chrome.browserAction.onClicked.addListener(function () {
     if (storage_data.user_token) {
       chrome.browserAction.setPopup({ popup: "html_files/normal.html" });
