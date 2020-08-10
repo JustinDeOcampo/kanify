78a6f8de46a8bbbb2c75e47895a821a1ff3f458b
Justin De Ocampo <justindeocampo99@gmail.com>
2020-08-10 15:58:10 -0700

    Cleaned up background.js

--- background_files/background.js
+++ background_files/background.js
@@ -1,16 +1,14 @@
 import { onAPIInputSubmit, onKanifyClick } from '../popup_scripts/buttonHandlers.js'
 import { kanifyClicker } from '../popup_scripts/interface_renderer.js';
 //Call the API checker function every 1 minutes if the user has already input a valid api token
-chrome.storage.sync.get("user_token", (storage_data) => {
+chrome.storage.sync.get(["user_token", "kanji", "page_refresh"], (storage_data) => {
   if (storage_data.user_token) {
     setInterval(onAPIInputSubmit, 1000 * 60 * 5);
   }
-})
-console.log("in the background")
-chrome.storage.sync.get(["kanji", "page_refresh"], function (storage_data) {
-  chrome.commands.onCommand.addListener(function (command){
-    if(command == "kanify_me"){
+  chrome.commands.onCommand.addListener(function (command) {
+    if (command == "kanify_me") {
       onKanifyClick()
     }
   })
-});
+})
+
